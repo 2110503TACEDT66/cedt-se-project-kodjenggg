7fc992c45b7bbfa97f6b2e2d20436fd44d0a67c4
TaksaphornNow <taksaphorn.tpd@gmail.com>
2024-04-11 22:08:01 +0700

    RatingStarts

---
+++ frontend/src/components/RatingStar.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { Rating } from '@mui/material';
+import { useState } from 'react';
+
+export function RatingStar () {
+    const [ rating, setRating ] = useState(0)
+    return (
+        <div>
+            <Rating className="w-full h-[10%]" value={(rating==undefined)? 0:rating}
+            onChange={(e,newValue)=>{if(newValue!=null) setRating(newValue)}}/>
+            <h1 className='text-black'>{`${rating}`}</h1>
+        </div>
+        
+        
+    )
+}

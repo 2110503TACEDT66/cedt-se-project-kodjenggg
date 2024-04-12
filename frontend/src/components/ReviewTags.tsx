10050f0e4dd2dbc69226ad89ce96eb9dfeaee09f
Wassamont <nene.nesaki@gmail.com>
2024-04-12 19:28:13 +0700

    create review tags

--- frontend/src/components/ReviewTags.tsx
+++ frontend/src/components/ReviewTags.tsx
@@ -5,11 +5,11 @@ export default function ReviewTags({tagsName, value, isUse}: {tagsName:string, v
         <div className="justify-center w-1/6">
             {
                 value?
-                <button className="px-3 py-1 text-xs text-white rounded-lg bg-[#F99417] h-[30px] border-2 border-[#F99417] w-[95%] top-2 
+                <button className="px-3 py-1 text-xs text-center text-white rounded-lg bg-[#F99417] h-[30px] border-2 border-[#F99417] w-[95%] top-2 
                 hover:bg-white hover:text-[#F99417]"
                 onClick={()=>{isUse(!value)}}>
                 {tagsName}</button>
-                :<button className=" px-3 py-1 text-xs text-[#F99417] rounded-lg bg-white h-[30px] border-2 border-[#F99417] w-[95%] top-2 
+                :<button className=" px-3 py-1 text-xs text-center text-[#F99417] rounded-lg bg-white h-[30px] border-2 border-[#F99417] w-[95%] top-2 
                 hover:bg-[#F99417] hover:text-white"
                 onClick={()=>{isUse(!value)}}>
                 {tagsName}</button>

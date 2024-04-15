45e49fe88fe9b055cd23a531d44990e065467b39
TaksaphornNow <taksaphorn.tpd@gmail.com>
2024-04-16 01:19:28 +0700

    ReviewHotel

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -5,7 +5,8 @@ const {
   updateReview ,
   updateReport ,
   updateReply,
-  addReview
+  addReview,
+  deleteReview
 } = require("../controllers/reviews");
 
 //Include other resource routers
@@ -20,8 +21,9 @@ router
 
 router
     .route("/:id")
-    .put(protect , authorize("user") ,updateReview)
+    .put(protect , authorize("user","admin") ,updateReview)
     .put(protect, authorize('hotelmanager' , updateReply))
+    .delete(protect, authorize("admin", "user"), deleteReview);
 router
     .route("/report/:id")
     .put(protect , authorize("user") , updateReport)

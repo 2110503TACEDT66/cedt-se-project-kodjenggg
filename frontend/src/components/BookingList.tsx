f55de647aea1972f1f4791b8e0c8ad3aba325723
Wassamont <nene.nesaki@gmail.com>
2024-04-19 09:39:02 +0700

    JUST GO PAY

--- frontend/src/components/BookingList.tsx
+++ frontend/src/components/BookingList.tsx
@@ -63,7 +63,7 @@ export default function BookingList ({session}:{session:any}) {
                             onClick={()=>{deleteReservations(session.user.token, reserve._id)}}>Delete</button>)}
                             {reserve.status === 'unpaid'&& (
                                 <button className="px-3 py-1 text-white shadow-sm rounded-xl bg-green-600 absolute h-[40px] w-[80px] right-4 bottom-3"
-                                onClick={() => { alert("GO PAY MOTHER FUCKER") }}>Pay</button>
+                                onClick={() => { alert("GO PAY") }}>Pay</button>
                             )}
                             {reserve.status === 'pending'&& (
                                 <div className="text-[#F99417] text-md absolute right-8 top-2">

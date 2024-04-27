df1f4a94a40446dc3e5823d7c6b29dc7a52d7c5c
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-04-27 18:38:04 +0700

    Payment confirmation(demo)

--- backend/controllers/reservations.js
+++ backend/controllers/reservations.js
@@ -83,10 +83,16 @@ exports.getReservations = async (req, res, next) => {
 // @access  Public
 exports.getReservation = async (req, res, next) => {
   try {
-    const reservation = await Reservation.findById(req.params.id).populate({
-      path: "hotel",
-      select: "name description tel",
-    });
+    const reservation = await Reservation.findById(req.params.id).populate([
+      {
+        path: "hotel",
+        select: "name province tel picture",
+      },
+      {
+        path: "user",
+        select: "name tel"
+      }
+    ]);
 
     if (!reservation) {
       return res.status(404).json({
--- frontend/src/app/todsoft/page.tsx
+++ frontend/src/app/todsoft/page.tsx
@@ -1,9 +1,33 @@
-import ReportPopup from "@/components/Reportpopup";
+'use client'
+import React, { useState } from 'react';
 
-export default function Todsoft () {
-    return (
-        <main>
-            <ReportPopup/>
-        </main>
-    )
+function App() {
+  const [binaryData, setBinaryData] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    // Ensure file is selected
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setBinaryData(reader.result); // Set the binary data as ArrayBuffer
+      };
+      reader.readAsArrayBuffer(file); // Read the file as an ArrayBuffer
+    }
+  };
+
+  return (
+    <div>
+      <h1>Image Upload</h1>
+      <input type="file" onChange={handleImageChange} />
+      {binaryData && (
+        <div>
+          <h2>Binary Data:</h2>
+          <p>{JSON.stringify(Array.from(new Uint8Array(binaryData)))}</p>
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default App;

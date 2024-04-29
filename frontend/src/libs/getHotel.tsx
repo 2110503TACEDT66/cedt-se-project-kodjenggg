50294bf25716f61a7b444b319b6402b43135b0ea
Wassamont <nene.nesaki@gmail.com>
2024-04-29 14:00:52 +0700

    final

--- frontend/src/libs/getHotel.tsx
+++ frontend/src/libs/getHotel.tsx
@@ -1,7 +1,9 @@
 import { resolve } from "path"
 
 export default async function getHotel(id:string) {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/hotels/${id}`)
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/hotels/${id}`, {
+        cache: 'no-store'
+    })
     if(!response.ok){
         console.log(response.json);
         throw new Error ('Failed to fetch hotel')
